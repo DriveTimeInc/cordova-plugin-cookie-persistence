7d4404e5ba26faf0f5a688b888c5af09c93a640a
Unknown <mmcdonald39@gmail.com>
2018-06-12 09:25:49 -0700

    refactor started

--- src/ios/CookiePersistencePlugin.m
+++ src/ios/CookiePersistencePlugin.m
@@ -18,6 +18,7 @@
 }
 
 
+
 // STORE TEXT
 - (void)storeText:(CDVInvokedUrlCommand*)command
 {
@@ -47,7 +48,7 @@
 
 -(void) overwriteTextFile:(NSString*)fileContents fileName:(NSString*)fileName
 {
-    NSString* filePath = [self getFilePathByname: fileName];
+    NSString* filePath = [self getFilePathByName: fileName];
 
     [fileContents writeToFile:filePath
                     atomically:NO
@@ -56,7 +57,121 @@
 
 }
 
--(NSString *) getFilePathByname: (NSString*)fileName
+- (void)storeCookies:(CDVInvokedUrlCommand*)command
+{
+    NSString* cookies = [[command arguments] objectAtIndex:0];
+
+    [self overwriteFile: cookies];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:@"cookies saved to disk"];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+- (void)storeLocalStorage:(CDVInvokedUrlCommand*)command
+{
+    NSString* localStorage = [[command arguments] objectAtIndex:0];
+
+    [self overwriteFile: localStorage];
+
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:@"localStorage saved to disk"];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+
+-(void) overwriteFile:(NSString*)fileContents
+{
+    NSString *fileName = [self getFileName];
+
+    [fileContents writeToFile:fileName
+                    atomically:NO
+                    encoding:NSUTF8StringEncoding
+                    error:nil];
+
+}
+
+
+
+
+
+
+
+
+// retrieveCookiesAndLocalStorage()
+// retrieveFile('cookies.txt');
+// retrieveFile('localstorage.txt');
+// return [cookies, localStorage]
+- (void)retrieveCookiesAndLocalStorage:(CDVInvokedUrlCommand*)command
+{
+    NSString* cookies = [self readFileByName:@"cookies.txt"];
+    NSString* localStorage = [self readFileByName:@"localstorage.txt"];
+
+    NSString *files[2];
+    files[0] = cookies;
+    files[1] = localStorage;
+
+    NSArray *res = [NSArray arrayWithObjects:files count:2];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsArray:res];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+// retrieveCookies()
+// retrieveFile('cookies.txt');
+// return cookies string
+- (void)retrieveCookies:(CDVInvokedUrlCommand*)command
+{
+    NSString* file = [self readFileByName:@"cookies.txt"];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:file];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+
+
+
+// retrieveLocalStorage()
+// retrieveFile('localstorage.txt');
+// return localStorage string
+- (void)retrieveLocalStorage:(CDVInvokedUrlCommand*)command
+{
+    NSString* file = [self readFileByName:@"localstorage.txt"];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:file];
+
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+// retrieveFile(name)
+// getFilePathByName(name);
+// getFileByPath(filePath)
+// return contents string
+-(NSString *) readFileByName:(NSString*)fileNameText
+{
+    NSString* filePath = [self getFilePathByName: fileNameText];
+
+    NSString *content = [[NSString alloc] initWithContentsOfFile:filePath
+                                            usedEncoding:nil
+                                            error:nil];
+
+    return content;
+}
+
+// checked
+-(NSString *) getFilePathByName: (NSString*)fileName
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains
                     (NSDocumentDirectory, NSUserDomainMask, YES);
@@ -70,46 +185,89 @@
 
 
 
+
+
+
+
+
+
 // GET TEXT
-- (void)retrieveStorage:(CDVInvokedUrlCommand*)command
-{
-    NSString* cookies = [self readFileByName: @"cookies.txt"];
+// - (void)retrieveStorage:(CDVInvokedUrlCommand*)command
+// {
+//     NSString* cookies = [self readFileByName: @"cookies.txt"];
 
-    NSString* localStorage = [self readFileByName: @"localstorage.txt"];
+//     NSString* localStorage = [self readFileByName: @"localstorage.txt"];
 
-    if (cookies != nil && localStorage != nil) {
-        NSLog(@"CookiePersistencePlugin - OC - retrieveCookies - %@", cookies);
+//     if (cookies != nil && localStorage != nil) {
+//         NSLog(@"CookiePersistencePlugin - OC - retrieveCookies - %@", cookies);
 
-        NSString *files[2];
-        files[0] = cookies;
-        files[1] = localStorage;
-        NSArray *res = [NSArray arrayWithObjects:files count:2];
+//         NSString *files[2];
+//         if (cookies) {
+//             files[0] = cookies;
+//         }
 
-        CDVPluginResult* result = [CDVPluginResult
-                                   resultWithStatus:CDVCommandStatus_OK
-                                   messageAsArray:res];
+//         if (localStorage) {
+//             files[1] = localStorage;
+//         }
 
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-    } else {
-        CDVPluginResult* result = [CDVPluginResult
-                                   resultWithStatus:CDVCommandStatus_OK
-                                   messageAsString:cookies];
+//         NSArray *res = [NSArray arrayWithObjects:files count:2];
 
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-    }
-}
+//         CDVPluginResult* result = [CDVPluginResult
+//                                    resultWithStatus:CDVCommandStatus_OK
+//                                    messageAsArray:res];
 
--(NSString *) readFileByName:(NSString*)fileNameText
-{
-    NSString* filePath = [self getFilePathByname: fileNameText];
+//         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+//     } else {
+//         CDVPluginResult* result = [CDVPluginResult
+//                                    resultWithStatus:CDVCommandStatus_OK
+//                                    messageAsString:@"No files found"];
 
-    NSString *content = [[NSString alloc] initWithContentsOfFile:filePath
-                                            usedEncoding:nil
-                                            error:nil];
+//         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+//     }
+// }
 
-    NSLog(@"readFileByName::%@",content);
 
-    return content;
-}
+// -(NSString *) readFileByName:(NSString*)fileNameText
+// {
+//     NSString* filePath = [self getFilePathByname: fileNameText];
+
+//     NSString *content = [[NSString alloc] initWithContentsOfFile:filePath
+//                                             usedEncoding:nil
+//                                             error:nil];
+
+//     NSLog(@"readFileByName::%@",content);
+
+//     return content;
+// }
+
+
+
+
+// -(NSString *) readFile
+// {
+//     NSString *fileName = [self getFileName];
+//     NSString *content = [[NSString alloc] initWithContentsOfFile:fileName
+//                                             usedEncoding:nil
+//                                             error:nil];
+
+//     NSLog(@"CONTENT:%@",content);
+
+//     return content;
+// }
+
+// -(NSString *) getFileName
+// {
+//     NSArray *paths = NSSearchPathForDirectoriesInDomains
+//                     (NSDocumentDirectory, NSUserDomainMask, YES);
+//     NSString *documentsDirectory = [paths objectAtIndex:0];
+
+//     NSString *fileName = [NSString stringWithFormat:@"%@/cookies.txt",
+//                                                     documentsDirectory];
+
+//     return fileName;
+// }
+
+
+
 
 @end
--- www/ios/CookiePersistencePlugin.js
+++ www/ios/CookiePersistencePlugin.js
@@ -8,10 +8,27 @@ console.log('CookiePersistencePlugin - JS - Cookies', document.cookie)
 //When this code is run, just get the cookie and place it in the window
 //But we need to notify when complete
 
-function restoreCookies(onSuccess, onError) {
+function restoreCookiesAndLocalStorage(onSuccess, onError) {
+  console.time("restoreCookies - start");
   fetchCookiesAndLocalStorage(function(res) {
     applyCookiesAndLocalStorageToBrowser(res);
     onSuccess(res);
+  }, onError);
+}
+
+function restoreCookies(onSuccess, onError) {
+  console.time("restoreCookies - start");
+  fetchCookies(function(cookies) {
+    refreshCookies(cookies);
+    onSuccess(cookies);
+  }, onError);
+}
+
+function restoreLocalStorage(onSuccess, onError) {
+  console.time("restoreCookies - start");
+  fetchLocalStorage(function(savedStorage) {
+    refreshLocalStorage(savedStorage);
+    onSuccess(savedStorage);
   }, onError);
 }
 
@@ -19,6 +36,7 @@ function fetchCookiesAndLocalStorage(onSuccess, onError) {
   console.log('CookiePersistencePlugin - JS - restoreCookies');
 
   var successCallback = function(res) {
+    console.timeEnd("restoreCookies - end");
     console.log('CookiePersistencePlugin - JS - restoreCookies successCallback called', res)
     onSuccess(res);
   }
@@ -28,57 +46,83 @@ function fetchCookiesAndLocalStorage(onSuccess, onError) {
     onError(e);
   }
 
-  cordova.exec(successCallback, errorCallback, "CookiePersistence", "retrieveStorage", []);
+  cordova.exec(successCallback, errorCallback, "CookiePersistence", "retrieveCookiesAndLocalStorage", []);
 }
 
-window.restoreCookies = restoreCookies;
-
 function applyCookiesAndLocalStorageToBrowser(res) {
-  var locStorage = res[1];
-  if (locStorage && locStorage.length > 0) {
-    var keyValuePairs = locStorage
-      .split(';')
-      .filter(function(item) {
-      return item !== "";
-    });
+  refreshCookies(res[0]);
+  refreshLocalStorage(res[1]);
+}
 
-    for (var i = 0; i < keyValuePairs.length; i++) {
-      var pair = keyValuePairs[i].split('=');
-      localStorage.setItem(pair[0], pair[1]);
-    }
+function refreshCookies(savedCookies) {
+  if (!savedCookies || savedCookies.length < 1) return;
+  savedCookies
+  .trim(' ')
+  .split(';')
+  .forEach(savedCookie =>
+    document.cookie = savedCookie
+  );
+}
+
+function refreshLocalStorage(savedStorage) {
+  if (!savedStorage || savedStorage.length < 1) return;
+
+  var parsedStorage = {};
+  try {
+    parsedStorage = JSON.parse(savedStorage);
+  } catch(e) {
+    throw e;
+    // come back to this? bubble it up
   }
 
-  var cookies = res[0];
-  if (cookies && cookies.length > 0) {
-    cookies
-      .trim(' ')
-      .split(';')
-      .forEach(cookie =>
-        document.cookie = cookie
-      );
+  Object.keys(parsedStorage).forEach(function(key) {
+    localStorage.setItem(key, parsedStorage[key]);
+  });
+}
+
+function getLocalStorageAsString() {
+
+  var localStorageObj = {}
+  for (var i = 0; i < localStorage.length; i++) {
+    localStorageObj[localStorage.key(i)] = localStorage.getItem(localStorage.key(i));
   }
+
+  return JSON.stringify(localStorageObj);
 }
 
 function writeCookies() {
   var cookies = document.cookie;
-
-  var localStorageText = "";
-  for (var i = 0; i < localStorage.length; i++) {
-    localStorageText +=  localStorage.key(i) + '=' + localStorage.getItem(localStorage.key(i)) + ';';
-  }
-
   console.log('CookiePersistencePlugin - JS - Pause', cookies);
-  console.log('CookiePersistencePlugin - JS - Pause', localStorageText);
 
-  var successCallback = r => {
+  var successCallback = function(r) {
     console.log('CookiePersistencePlugin - JS - Pause successCallback called', r)
   }
 
-  var errorCallback = e => {
+  var errorCallback = function(e) {
     console.log('CookiePersistencePlugin - JS - Pause errorCallback called', e)
   }
 
-  cordova.exec(successCallback, errorCallback, "CookiePersistence", "storeText", [["cookies.txt", cookies], ["localstorage.txt", localStorageText]]);
+  cordova.exec(successCallback, errorCallback, "CookiePersistence", "storeCookies", [cookies]);
 }
 
+function writeLocalStorage() {
+  var localStorageText = getLocalStorageAsString();
+  console.log('CookiePersistencePlugin - JS - Pause', localStorageText);
+
+  var successCallback = function(r) {
+    console.log('CookiePersistencePlugin - JS - Pause successCallback called', r)
+  }
+
+  var errorCallback = function(e) {
+    console.log('CookiePersistencePlugin - JS - Pause errorCallback called', e)
+  }
+
+  cordova.exec(successCallback, errorCallback, "CookiePersistence", "storeLocalStorage", [localStorageText]);
+}
+
+window.restoreCookies = restoreCookies;
+window.restoreLocalStorage = restoreLocalStorage;
+window.restoreCookiesAndLocalStorage = restoreCookiesAndLocalStorage;
+
 channel.onPause.subscribe(writeCookies)
+channel.onPause.subscribe(writeLocalStorage);
